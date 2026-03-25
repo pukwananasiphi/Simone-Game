9b77d13488a5443c0f70110c8c5755d1aee04780
Nasiphi Pukwana <NasiphiP@tfg.co.za>
2026-03-25 13:43:58 +0200

    Add button flash animation

--- game.js
+++ game.js
@@ -5,5 +5,6 @@ function nextSequence(){
     var randomNumber= Math.floor(Math.random()*4);
     var randomChosenColor = bottonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
 }
