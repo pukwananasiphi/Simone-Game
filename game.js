86a5b00936101c8329afdf8a8a9b34e52385de32
Nasiphi Pukwana <NasiphiP@tfg.co.za>
2026-03-25 14:38:00 +0200

    Add animated press function

--- game.js
+++ game.js
@@ -1,22 +1,48 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
-
-
 var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+  if( !started){
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
 function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  userClickedPattern = [];
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-
-
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
